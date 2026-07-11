726cd1cff574665dd7237552556bec3b1ab0203a
LO <lo@drainwise.app>
2026-07-11 17:23:49 +0000

    Full layout + NavBar - test if 500 returns

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
   import { onMount } from 'svelte';
-  import { page } from '$app/stores';
+  import { initAuth } from '$lib/stores/auth';
   import '../app.css';
+  import NavBar from '$lib/components/NavBar.svelte';
 
-  onMount(() => {});
+  onMount(() => initAuth());
 </script>
 
+<NavBar />
 <main class="max-w-4xl mx-auto px-4 py-6">
   <slot />
 </main>
